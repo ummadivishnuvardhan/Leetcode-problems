aa71da5649f4c83d099f04be8f01f6011195f8d0
ummadivishnuvardhan <ummadivishnuvardhan46@gmail.com>
2025-07-13 11:41:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> al=new ArrayList<>();
+        subset(nums,res,al,0);
+        return res;
+    }
+    public void subset(int[] nums,List<List<Integer>> res,List<Integer> al,int count){
+        if(count==nums.length){
+            res.add(new ArrayList<>(al));
+            return;
+        }
+
+        al.add(nums[count]);
+        subset(nums,res,al,count+1);
+        al.remove(al.size()-1);
+         while (count + 1 < nums.length && nums[count] == nums[count + 1]) {
+            count++;
+        }
+        subset(nums,res,al,count+1);
+    }
+}
