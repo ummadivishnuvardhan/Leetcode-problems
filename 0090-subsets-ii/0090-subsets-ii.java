1c2825d8ed3a810974eddfe593e1a92d259239d1
ummadivishnuvardhan <ummadivishnuvardhan46@gmail.com>
2025-07-13 11:59:37 +0530

    Time: 8 ms (8.54%), Space: 43.8 MB (29.58%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -8,16 +8,21 @@ class Solution {
     }
     public void subset(int[] nums,List<List<Integer>> res,List<Integer> al,int count){
         if(count==nums.length){
-            res.add(new ArrayList<>(al));
-            return;
+            if(res.contains(al)){
+                return;
+            }
+            else{
+                res.add(new ArrayList<>(al));
+                return;
+            }
         }
 
         al.add(nums[count]);
         subset(nums,res,al,count+1);
         al.remove(al.size()-1);
-         while (count + 1 < nums.length && nums[count] == nums[count + 1]) {
-            count++;
-        }
+        //  while (count + 1 < nums.length && nums[count] == nums[count + 1]) {
+        //     count++;
+        // }
         subset(nums,res,al,count+1);
     }
 }
