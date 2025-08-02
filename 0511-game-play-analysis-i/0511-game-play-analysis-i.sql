13d296f79dd82aafcb41504ee900578fc1024496
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-08-02 14:39:04 +0530

    Time: 535 ms (29.41%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -2,4 +2,4 @@
 -- select distinct e1.player_id,e1.event_date from Activity e1 join Activity e2
 -- on DATEDIFF(e1.event_date,e2.event_date)>=1;
 
-select player_id,event_date as first_login from Activity group by player_id;
+select player_id,MIN(event_date) as first_login from Activity group by player_id;
