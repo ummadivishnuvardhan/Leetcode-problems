764c806a16df0f1962e80c1debd3f2e13f91f211
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-08-02 14:36:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+-- select distinct e1.player_id,e1.event_date from Activity e1 join Activity e2
+-- on DATEDIFF(e1.event_date,e2.event_date)>=1;
+
+select player_id,event_date as first_login from Activity group by player_id;
