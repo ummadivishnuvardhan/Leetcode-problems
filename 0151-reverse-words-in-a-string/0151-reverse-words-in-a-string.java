0e2f748e2d48aadab14c5af6464624cbdd04ae4e
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-08-09 23:40:57 +0530

    Time: 11 ms (18.87%), Space: 45 MB (21.81%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,30 +1,41 @@
 class Solution {
     public String reverseWords(String s) {
-        int n=s.length()-1;
-        StringBuilder ans=new StringBuilder();
-        s=reverse(0,n,s).toString();
-        for(int i=0;i<=n;i++){
-           StringBuilder word=new StringBuilder();
-           while(i<=n&&s.charAt(i)!=' '){
-            word.append(s.charAt(i));
-            i++;
-           }
-           word=reverse(0,word.length()-1,word.toString());
-           if(word.length()>0){
-            ans.append(" ");
-            ans.append(word);
-           }
-            
-        }
-        return ans.substring(1).toString();
+        s = s.trim(); // Remove leading/trailing spaces
+        int n = s.length() - 1;
+        StringBuilder ans = new StringBuilder();
 
+        // Step 1: Reverse the whole string
+        s = reverse(0, n, s).toString();
+
+        // Step 2: Reverse each word
+        for (int i = 0; i <= n; i++) {
+            StringBuilder word = new StringBuilder();
+
+            // Build the word
+            while (i <= n && s.charAt(i) != ' ') {
+                word.append(s.charAt(i));
+                i++;
+            }
+
+            // Reverse the word
+            word = reverse(0, word.length() - 1, word.toString());
+
+            // Append word to result without leading extra space
+            if (word.length() > 0) {
+                if (ans.length() > 0) ans.append(" ");
+                ans.append(word);
+            }
+        }
+
+        return ans.toString();
     }
-    public StringBuilder reverse(int left,int right,String s){
-        StringBuilder str=new StringBuilder(s);
-        while(left<right){
-            char ch1=str.charAt(left);
-           str.setCharAt(left,str.charAt(right));
-           str.setCharAt(right,ch1);
+
+    public StringBuilder reverse(int left, int right, String s) {
+        StringBuilder str = new StringBuilder(s);
+        while (left < right) {
+            char ch1 = str.charAt(left);
+            str.setCharAt(left, str.charAt(right));
+            str.setCharAt(right, ch1);
             left++;
             right--;
         }
