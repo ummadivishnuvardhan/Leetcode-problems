9a5f9be1191be87826e84f52ceb88ff25231a1aa
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-08-18 20:28:48 +0530

    Time: 7 ms (99.98%), Space: 49.2 MB (87.54%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,7 +1,7 @@
 class NumArray {
     int prefix[];
     public NumArray(int[] nums) {
-       this.prefix=nums;
+       prefix=new int[nums.length];
         prefix[0]=nums[0];
         for(int i=1;i<nums.length;i++){
             prefix[i]=nums[i]+prefix[i-1];
