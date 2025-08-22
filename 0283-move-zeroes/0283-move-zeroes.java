31563fe8c1e6601054ec222c75e1cbc922178b03
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-08-22 22:22:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length==1||nums.length==0) return;
+        int i=0,j=0;
+        while(j<nums.length){
+            if(nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+            else{
+                while(j<nums.length&&nums[j]==0){
+                    j++;
+                }
+            }
+        }
+    }
+}
