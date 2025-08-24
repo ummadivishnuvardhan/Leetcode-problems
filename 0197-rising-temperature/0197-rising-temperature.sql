a5c20ccfb27c697fbc9caf2f5d3b1f5bfa8b7651
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-08-24 15:58:02 +0530

    Time: 474 ms (64.45%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,3 @@
-select w1.id from weather w1 join weather w2
-on DATEDIFF(w1.recordDate,w2.recordDate)=1
-where w1.temperature>w2.temperature;
-
+select w1.id from Weather as w1 , Weather as w2 
+where DATEDIFF(w1.recordDate,w2.recordDate)=1 and
+w1.temperature>w2.temperature;
