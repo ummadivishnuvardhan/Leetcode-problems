aad8a65bfa85e22603f51abe481f4038a8d166cf
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-08-25 23:11:39 +0530

    Time: 0 ms (100%), Space: 42.6 MB (42.45%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        int low=0,high=nums.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+          
+            
+            if(nums[mid]>nums[mid+1]){
+               high=mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return  low;
+    }
+}
