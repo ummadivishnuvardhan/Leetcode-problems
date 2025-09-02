bd5f9eed5706b4bd80e08f51e96816a2fd8f5143
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-09-02 21:19:51 +0530

    Time: 1 ms (48.76%), Space: 43.1 MB (10.11%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,18 +2,17 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result=new ArrayList<>();
         List<Integer> al=new ArrayList<>();
-       // List<List<Integer>> right=new ArrayListList<>();
-        subset(nums,result,al,0);
-        return result;
+       subset(nums,result,al,0);
+       return result;
     }
     public void subset(int[] nums,List<List<Integer>> result,List<Integer> al,int count){
-            if(count==nums.length){
-                result.add(new ArrayList<>(al));
-                return ;
-            }
-            al.add(nums[count]);
-            subset(nums,result,al,count+1);
-            al.remove(al.size()-1);
-            subset(nums,result,al,count+1);
+        if(count==nums.length){
+            result.add(new ArrayList(al));
+            return ;
+        }
+        al.add(nums[count]);
+        subset(nums,result,al,count+1);
+        al.remove(al.size()-1);
+        subset(nums,result,al,count+1);
     }
 }
