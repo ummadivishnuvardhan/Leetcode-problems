06a0a0ed1f436f3467fdff32905743dbafea3530
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2025-10-15 20:55:29 +0530

    Time: 3 ms (56.1%), Space: 42.2 MB (23.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        
+        Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='['||ch=='{'||ch=='('){
-                stack.push(ch);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
             }
-            else if(ch=='}'||ch==']'||ch==')'){
-                if(!stack.isEmpty()){
-                    if((stack.peek()=='['&&ch==']')||(stack.peek()=='{'&&ch=='}')||(stack.peek()=='('&&ch==')')){
-                        stack.pop();
-                    }
-                    else return false;
-                }
-                 else return false;
+            else if(ch==')'||ch==']'||ch=='}'){
+                if(st.isEmpty()) return false;
+                    char top=st.peek();
+                    if(top=='('&&ch==')') st.pop();
+                    else if(top=='{'&&ch=='}') st.pop();
+                    else if(top=='['&&ch==']') st.pop();
+                    else break;
+                
             }
-            
         }
-      return stack.isEmpty();
-        
+        return st.isEmpty();
     }
 }
