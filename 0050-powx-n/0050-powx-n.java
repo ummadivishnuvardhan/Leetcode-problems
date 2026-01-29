144fac52508e0d4fc4752d1eafba34f1953eb5a0
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2026-01-29 13:40:28 +0530

    Time: 0 ms (100%), Space: 48.5 MB (8.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,26 +1,26 @@
 class Solution {
     public double myPow(double x, int n) {
-      long nn=0;
-      if(n<0){
-        nn=-n;
-      }
-      else{
-        nn=n;
-      }
-      double res=pow(x,nn);
-      if(n<0){
-        return 1/res;
-      }
-      return res;
-    }
-    public double pow(double x,long n){
-        if(n==0) return 1;
-        double half=pow(x,n/2);
-        if(n%2==0){
-            return half*half;
+        double ans = 1;
+        long m = n;      // CHANGED
+        long n1 = n;     // CHANGED
+
+        if (n1 < 0) {
+            n1 = n1 * -1;
         }
-        else{
-            return half*half*x;
+
+        while (n1 > 0) {
+            if (n1 % 2 == 1) {
+                ans = ans * x;
+                n1 = n1 - 1;
+            } else {
+                x = x * x;
+                n1 = n1 / 2;
+            }
         }
+
+        if (m < 0) {
+            return 1.0 / ans;
+        }
+        return ans;
     }
 }
