1ba37d78689489aad0a5e3fd6600e80f99846811
Ummadi Vishnu Vardhan <ummadivishnuvardhan46@gmail.com>
2026-01-30 13:22:22 +0530

    Time: 2 ms (89.97%), Space: 47.9 MB (30.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,14 +2,17 @@ class Solution {
     public void moveZeroes(int[] arr) {
         if(arr.length==1||arr.length==0) return;
         int left=0,right=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-              int temp=arr[left];
+        while(right<arr.length){
+            if(arr[right]!=0){
+                int temp=arr[left];
                 arr[left]=arr[right];
                 arr[right]=temp;
                 left++;
             }
             right++;
         }
+       
     }
 }
+
+  
